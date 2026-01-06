94c6741f9635c3387a0d8399279679e1a14bf5a2
jakkli-mend <117362921+jakkli-mend@users.noreply.github.com>
2026-01-06 12:16:15 +0100

    Update sqli2.py

--- sqli2.py
+++ sqli2.py
@@ -11,7 +11,7 @@ def login():
   # Execute the vulnerable SQL query concatenating user-provided input.
   cursor.execute("SELECT * FROM users WHERE username = '%s' AND password = '%s'" % (username, password))
 
-  # If the query returns any matching record, considsdfger the current user logged in.
+  # If the query returns any matching record, considsdfger the currentgsdfgsd user logged in.
   record = cursor.fetchone()
   if record:
     session['logged_user'] = username
@@ -19,3 +19,4 @@ def login():
   # disconnect from server
 
   db.close()
+
